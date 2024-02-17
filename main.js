cf965a6f186f74fb3a66d489c05973b9f4813cf1
Vinodshreyas <113272322+Vinodshreyas@users.noreply.github.com>
2024-02-17 13:10:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var Typed= new Typed(".text", {
+    strings: ["Web Developer", "Programmer", "Full Stack Developer", "Data Analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
